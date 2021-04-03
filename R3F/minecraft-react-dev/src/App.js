4bc758e88742b61a18cecfa8ab57f084169a00ea
mivebe <mivebe@abv.bg>
2021-04-03 12:26:43 +0300

    no idea what i was doing back here

--- R3F/minecraft-react-dev/src/App.js
+++ R3F/minecraft-react-dev/src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { Sky } from "drei"
+import { Sky, Loader } from "drei"
 import { Physics } from "use-cannon";
 
 import { Ground } from "./components/Ground"
 import { Player } from "./components/Player"
+import { Cube } from "./components/Cube"
+import { log } from 'three';
 
 function App() {
-  return <Canvas shadowMap sRGB>
-    <Sky sunPosition={[100, 20, 100]} />
-    <ambientLight intensity={0.25} />
-    <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
-    <Physics gravity={[0, -30, 0]}>
-      <Ground position={[0, 0.5, 0]} />
-      <Player position={[0, 3, 10]} />
-    </Physics>
-  </Canvas>;
+  const firstTime = useRef(true)
+
+  useEffect(() => {
+    console.log(firstTime.current)
+    if (firstTime.current) {
+      firstTime.current = false
+    }
+  }, [])
+
+  return (<>
+    <Canvas onContextMenu={(e) => { e.preventDefault(); return false }} shadowMap sRGB>
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={0.25} />
+      <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
+      <Physics gravity={[0, -30, 0]}>
+        <Ground position={[0, 0.5, 0]} />
+        <Suspense>
+          <Player position={[0, 3, 10]} />
+        </Suspense>
+        <Cube position={[0, 1, 0]} type="wood" />
+      </Physics>
+    </Canvas>
+    {firstTime.current === true ? <Loader /> : () => { console.log("asd") }}
+  </>
+  )
 }
 
 export default App;
--- R3F/minecraft-react-dev/src/components/Player.js
+++ R3F/minecraft-react-dev/src/components/Player.js
@@ -1,21 +1,70 @@
-import React from "react";
-import { useSphere } from "use-cannon";
-import { useThree, useFrame } from "react-three-fiber";
+import React, { useEffect, useRef } from 'react';
+import { useBox, useSphere } from 'use-cannon';
+import { useThree, useFrame } from 'react-three-fiber';
+import { FPVControls } from './FPVControls';
+import { useKeyboardControls } from '../hooks/useKeyboardControls';
+import { Vector3 } from 'three';
+import { useGLTFLoader } from 'drei';
+
+const SPEED = 6;
 
 export const Player = (props) => {
     const { camera } = useThree();
-    const [ref] = useSphere(() => ({
+    const {
+        moveForward,
+        moveBackward,
+        moveLeft,
+        moveRight,
+        jump,
+    } = useKeyboardControls();
+    const [ref, api] = useBox(() => ({
         mass: 1,
-        type: "Dynamic",
+        type: 'Dynamic',
         ...props,
     }));
+    // const asd = useGLTFLoader("../../../warrior.glb")
+    // console.log(asd);
+
+    const velocity = useRef([0, 0, 0]);
+    useEffect(() => {
+        return api.velocity.subscribe((v) => (velocity.current = v));
+    }, [api.velocity]);
 
     useFrame(() => {
-        camera.position.copy(ref.current.position)
+        camera.position.copy(ref.current.position);
+        camera.position.z += 5
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0),
+        );
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0,
+        );
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation);
+
+        api.velocity.set(direction.x, velocity.current[1], direction.z);
+
+        if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) {
+            api.velocity.set(velocity.current[0], 8, velocity.current[2]);
+        }
     });
     return (
         <>
-            <mesh ref={ref} />
+            <FPVControls />
+            <mesh ref={ref} >
+                <boxBufferGeometry attach="geometry" />
+                <meshBasicMaterial color="blue" attach="material" />
+            </mesh>
         </>
-    )
-}
+    );
+};
