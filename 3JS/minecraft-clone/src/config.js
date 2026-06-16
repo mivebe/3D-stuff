e3e6d03daa4f2cbea453d56d176229d471b8b426
mivebe <mihailbezev10@gmail.com>
2026-06-16 14:47:02 +0300

    feat: world generation, trees and water rendering

--- 3JS/minecraft-clone/src/config.js
+++ 3JS/minecraft-clone/src/config.js
@@ -5,7 +5,8 @@ export const DEF_WORLD_SIZE = DEF_BPS * 2; // Size of the world in blocks
 export const DEF_HEIGHT = 64; // Height of the world in blocks
 
 export const DEF_FOV = 75; // Default field of view for the camera
-export const DEF_ASPECT = window.innerWidth / window.innerHeight; // Default aspect ratio
+export const DEF_ASPECT =
+  typeof window !== 'undefined' ? window.innerWidth / window.innerHeight : 1; // Default aspect ratio
 export const DEF_NEAR = 0.1; // Near clipping plane
 export const DEF_FAR = 1000; // Far clipping plane
 
